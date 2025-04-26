4ab04f73c46bf2f5a02f48ecf396282b05227175
Abhinandan <abhimehta1324@gmail.com>
2025-04-27 01:52:23 +0200

    line tether

--- LocalMultiplayerGame/Assets/Scripts/Tether.cs
+++ LocalMultiplayerGame/Assets/Scripts/Tether.cs
@@ -6,10 +6,16 @@ public class Tether : MonoBehaviour
     public Transform player2;
     private LineRenderer line;
 
+    public float maxStretchDistance = 7f;
+    public float warningStretchDistance = 5f;
+    public Color normalColor = Color.cyan;
+    public Color warningColor = Color.yellow;
+    public Color breakColor = Color.red;
+
     void Start()
     {
         line = GetComponent<LineRenderer>();
-        line.positionCount = 2; 
+        line.positionCount = 2;
     }
 
     void Update()
@@ -18,6 +24,33 @@ public class Tether : MonoBehaviour
         {
             line.SetPosition(0, player1.position);
             line.SetPosition(1, player2.position);
+
+            float distance = Vector3.Distance(player1.position, player2.position);
+
+            if (distance > maxStretchDistance)
+            {
+                Debug.Log("Link Broken!");
+                SetLineColor(breakColor);
+             
+            }
+            else if (distance > warningStretchDistance)
+            {
+                SetLineColor(warningColor);
+            }
+            else
+            {
+                SetLineColor(normalColor);
+            }
         }
+    }
+
+    void SetLineColor(Color color)
+    {
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(color, 0.0f), new GradientColorKey(color, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
+        );
+        line.colorGradient = gradient;
     }
 }
