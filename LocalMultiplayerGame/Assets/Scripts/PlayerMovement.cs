ccfb9c8dff909810bf6faed148872bf494ad29b7
Abhinandan <abhimehta1324@gmail.com>
2025-04-27 01:56:36 +0200

    goofy jumps added

--- LocalMultiplayerGame/Assets/Scripts/PlayerMovement.cs
+++ LocalMultiplayerGame/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Vector3 movement;
 
+    public float jumpForce = 5f;
+    private bool isGrounded = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,6 +27,10 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetKey(KeyCode.D)) moveX = 1f;
             if (Input.GetKey(KeyCode.W)) moveZ = 1f;
             if (Input.GetKey(KeyCode.S)) moveZ = -1f;
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                Jump();
+            }
         }
         else
         {
@@ -31,13 +38,31 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetKey(KeyCode.RightArrow)) moveX = 1f;
             if (Input.GetKey(KeyCode.UpArrow)) moveZ = 1f;
             if (Input.GetKey(KeyCode.DownArrow)) moveZ = -1f;
+            if (Input.GetKeyDown(KeyCode.RightControl) && isGrounded)
+            {
+                Jump();
+            }
         }
 
         movement = new Vector3(moveX, 0f, moveZ).normalized;
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts[0].normal == Vector3.up)
+        {
+            isGrounded = true;
+        }
+    }
+
 }
